64cfce6497e8f89dd8d403b1d92ab85aecd29b2e
Wolfgang Silbermayr <w.silbermayr@opentalk.eu>
2025-03-28 20:52:14 +0100

    feat: add client crate

---
+++ crates/http-request-derive-client/src/client.rs
@@ -0,0 +1,18 @@
+// SPDX-FileCopyrightText: OpenTalk GmbH <mail@opentalk.eu>
+//
+// SPDX-License-Identifier: MIT OR Apache-2.0
+
+use http_request_derive::HttpRequest;
+
+/// A client that can execute [`http_request_derive::HttpRequest`]s.
+#[async_trait::async_trait]
+pub trait Client {
+    /// An error that can be returned during request execution by the [`Client`].
+    type ClientError: std::error::Error;
+
+    /// Execute a [`http_request_derive::HttpRequest`], and read the typed response.
+    async fn execute<R: HttpRequest + Send>(
+        &self,
+        request: R,
+    ) -> Result<R::Response, Self::ClientError>;
+}
